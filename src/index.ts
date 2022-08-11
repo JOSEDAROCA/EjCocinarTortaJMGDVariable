f44cf8c0a071e72512875dfdf671a6a5916bee78
JOSE MARIA GARCIA DAROCA <110504961+JOSEDAROCA@users.noreply.github.com>
2022-08-11 09:36:12 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>`;
+
+var recetaTortaJMGD = [
+   "Receta Torta JMGD"
+ "Tomar un huevo y cascarlo separando la yema y la clara en un bowl tirar la cascara a la basura"
+ "Tomar un paquete de harina, abrirlo y verter el contenido en una taza hasta completar la mitad d la misma"
+ "Verter el contenido d la media taza de harina en el bowl en el que estan los huevos"
+ "Tomar un batidor manual y con movimientos envolventes mezclar el contenido del bowl por 3 minutos"
+ "Tomar un molde de aluminio de 30 cm de diametro y rociar su cara interna con aceite en aerosol "
+ "Verter la mezcla generada en el bowl en un molde"
+ "Meter el molde en un horno precalentado a 180 grados "
+ "Dejar en el horno por 40 minutos"
+ "Retirar del Horno y dejar enfrir por un lapso de 20 minutos"
+ "Invertir el molde sobre un plato y desmoldar"
+ "Servir y comer a gusto"
+]
+
+for(let i=0; i<recetaTortaJMGD.length; i++){
+  let mostrarReceta = (recetaTortaJMGD[i])
+
+console.log (mostrarReceta)
+}
+
+
+
+
